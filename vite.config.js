aebbba4b0cf173c14ceab77fb10f553bffbb5e58
Jeff LANCE <email@jefflance.me>
2025-12-12 17:36:52 +0100

    up

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   root: './public',
   base: '/pole-socle-numerique/progression_ia/',
-  publicDir: 'data',
   build: {
     outDir: '../build',
     emptyOutDir: true,
   },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'data',
+          dest: ''
+        }
+      ]
+    })
+  ]
 })
