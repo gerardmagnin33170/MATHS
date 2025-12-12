e9b097b208a2e4accc83acf51aff303a24ca9070
Jeff LANCE <email@jefflance.me>
2025-12-12 16:44:47 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  root: './public',
   base: '/progression_ia/',
+  build: {
+    outDir: '../build',
+    emptyOutDir: true,
+  },
 })
