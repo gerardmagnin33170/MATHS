2cbe0a5ea399486c59ff41f46cc3cfc9fe17e463
Jeff LANCE <email@jefflance.me>
2026-03-23 11:50:00 +0100

    fix: assets dir was missing

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,10 @@ export default defineConfig({
         {
           src: 'data',
           dest: ''
+        },
+        {
+          src: 'assets',
+          dest: ''
         }
       ]
     })
